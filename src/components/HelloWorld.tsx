90b7e937738fb5aa4a2ca4520d69935e861ba442
juliadekker <juliadekker11@hotmail.nl>
2024-07-21 16:57:11 +0200

    Hello World

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,12 @@
+import GreenLogo from '../assets/green-logo.png';
+
+const HelloWorld = () => {
+  return (
+    <div className='hello-world-container'>
+      <img src={GreenLogo} alt='RealEstateCare' />
+      <h2>Hello world!</h2>
+    </div>
+  );
+};
+
+export default HelloWorld;
